4d842b5d15a5e7abef0a38bf34a787a32e901161
Gabriel Tellez <tellez.gabriel1709@gmail.com>
2024-03-27 18:55:30 -0700

    Initial Commit

---
+++ src/controllerNode.cpp
@@ -0,0 +1,77 @@
+#include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include <SDL.h>
+
+const float MAX_JOYSTICK_VALUE = 32767.0;
+const float MIN_JOYSTICK_VALUE = -32767.0;
+
+
+using namespace std::chrono_literals;
+
+class ControllerNode : public rclcpp::Node
+{
+    public:
+    ControllerNode() : Node("ControllerNode")
+    {
+        // Open joystick controller
+        if(SDL_Init(SDL_INIT_JOYSTICK) != 0){
+            fprintf(stderr, "%s",SDL_GetError());
+            throw std::runtime_error("Failed to connect to controller");
+        }
+        
+        // Open the joystick device
+        this->joystick = SDL_JoystickOpen(0);
+        if(!this->joystick){
+            SDL_Quit();
+            throw std::runtime_error("Failed to open joystick");
+        }
+        this->publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
+        this->timer_ = this->create_wall_timer(500ms, std::bind(&ControllerNode::publishTwist, this));
+        this->counter_ = 0;
+    }
+    ~ControllerNode(){
+        SDL_JoystickClose(this->joystick);
+        SDL_Quit();
+    }
+    
+    void publishTwist()
+    {
+        float linear_speed;
+        float angular_velocity;
+        auto twist_msg = std::make_shared<geometry_msgs::msg::Twist>();
+        
+        while(rclcpp::ok()){
+            while(SDL_PollEvent(&this->event)){
+                if(this->event.type == SDL_JOYAXISMOTION){
+                    if(this->event.jaxis.axis == 0){
+                        linear_speed = SDL_JoystickGetAxis(this->joystick, 1) / MAX_JOYSTICK_VALUE;
+                    }else if(this->event.jaxis.axis == 3){
+                        angular_velocity = SDL_JoystickGetAxis(this->joystick, 2) / MAX_JOYSTICK_VALUE;
+                    }
+                }else {
+                    linear_speed = 0.0;
+                    angular_velocity = 0.0;
+                }
+                twist_msg->linear.x = -linear_speed;
+                twist_msg->angular.z = angular_velocity;
+                RCLCPP_INFO(get_logger(), "linear.=%f, angular.%f", twist_msg->linear.x, twist_msg->angular.z);
+                this->publisher_->publish(*twist_msg);
+            }
+        }
+    }
+    private:
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
+    rclcpp::TimerBase::SharedPtr timer_;
+    size_t counter_;
+    SDL_Event event;
+    SDL_Joystick* joystick;
+};
+
+int main(int argc, char *argv[])
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<ControllerNode>();
+    node->publishTwist();
+    rclcpp::shutdown();
+    return 0;
+}
